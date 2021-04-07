4767016152ae5ed550bf0fd90ee081360b4cd2ea
Ulices Esqueda <uesqueda756@gmail.com>
2021-04-07 12:22:47 -0700

    Added some documentation to UploadMusicDL

--- UploadMusicService/UploadMusicDL/UploadMusicRepoDB.cs
+++ UploadMusicService/UploadMusicDL/UploadMusicRepoDB.cs
@@ -9,6 +9,10 @@ using System.Xml.Linq;
 
 namespace UploadMusicDL
 {
+    /// <summary>
+    /// This class as a whole has all the necessary functionality for uploading music object to our database with all the basic CRUD operations 
+    /// with some special cases for the reading/get methods.
+    /// </summary>
     public class UploadMusicRepoDB : IUploadMusicRepoDB
     {
         private readonly UploadMusicDBContext _context;
@@ -17,6 +21,8 @@ namespace UploadMusicDL
         {
             _context = context;
         }
+
+        //Adding a Upload music object to the database
         public async Task<UploadMusic> AddUploadedMusicAsync(UploadMusic newUploadedMusic)
         {
             await _context.UploadMusic.AddAsync(newUploadedMusic);
@@ -24,6 +30,7 @@ namespace UploadMusicDL
             return newUploadedMusic;
         }
 
+        // Deleting a Upload music object to the database
         public async Task<UploadMusic> DeleteUploadedMusicAsync(UploadMusic uploadedMusic2BDeleted)
         {
             _context.UploadMusic.Remove(uploadedMusic2BDeleted);
@@ -31,11 +38,13 @@ namespace UploadMusicDL
             return uploadedMusic2BDeleted;
         }
 
+        // Getting all Uploaded music from database
         public async Task<List<UploadMusic>> GetUploadedMusicAsync()
         {
             return await _context.UploadMusic.ToListAsync();
         }
 
+        // Get a Uploaded music object by Id
         public async Task<UploadMusic> GetUploadedMusicByIDAsync(int id)
         {
             return await _context.UploadMusic
@@ -43,6 +52,7 @@ namespace UploadMusicDL
                 .FirstOrDefaultAsync();
         }
 
+        // Get Uploaded music by user Id
         public async Task<List<UploadMusic>> GetUploadedMusicByUserIDAsync(int userid)
         {
             return await _context.UploadMusic
@@ -50,6 +60,7 @@ namespace UploadMusicDL
                 .ToListAsync();
         }
 
+        // Updating a uploaded music object by getting the old object, setting its information with the new object passed into it, and then saving it to the database
         public async Task<UploadMusic> UpdateUploadedMusicAsync(UploadMusic uploadedMusic2BUpdated)
         {
             UploadMusic oldUploadedMusic = await _context.UploadMusic.Where(um => um.Id == uploadedMusic2BUpdated.Id).FirstOrDefaultAsync();
