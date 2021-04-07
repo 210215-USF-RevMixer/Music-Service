b31b7155636875d11cba65d3d8ae4c109f0d5098
Steven Imas <StevenImas11@hotmail.com>
2021-04-07 16:51:00 -0400

    wrote ticketed tests

--- UploadMusicService/UploadMusicTest/UploadMusicTest.cs
+++ UploadMusicService/UploadMusicTest/UploadMusicTest.cs
@@ -1,27 +1,128 @@
-using System;
-using Xunit;
-namespace UploadMusicTest
-{
-    public class UploadMusicTest() 
-    {
-        private readonly DnContextOptions<DBCONTEXT> options;
 
-        public UploadMusicTest()
+using Microsoft.AspNetCore.Mvc;
+using Moq;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using UploadMusicBL;
+using UploadMusicModels;
+using UploadMusicREST.Controllers;
+
+namespace MixerTests
+{
+    public class UploadedMusicControllerTest
+    {
+        private Mock<IUploadMusicBL> _uploadMusicBLMock;
+
+        public UploadedMusicControllerTest()
         {
-            options = new DbContextOptionsBuilder<DBCONTEXT>()
-                .UseSqlite("Filename=Test.db")
-                .Options;
-            Seed();
+            _uploadMusicBLMock = new Mock<IUploadMusicBL>();
+        }
+
+
+
+        [Fact]
+        public async Task DeleteUploadedMusicAsync_ShouldReturnStatusCode500_WhenInvalid()
+        {
+            //arrange
+            int uploadMusicID = -2;
+            UploadMusic uploadMusic = null;
+            _uploadMusicBLMock.Setup(x => x.DeleteUploadedMusicAsync(uploadMusic)).Throws(new Exception());
+            UploadMusicController uploadedMusicController = new UploadMusicController(_uploadMusicBLMock.Object);
+
+            //act
+            var result = await uploadedMusicController.DeleteUploadedMusicAsync(uploadMusicID);
+
+            //assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsInstanceOfType(result, typeof(StatusCodeResult));
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.AreEqual(500, ((StatusCodeResult)result).StatusCode);
         }
 
         [Fact]
-        public void test1()
+        public async Task GetUploadedMusicById_ShouldReturnOKObjectResult_WhenIDIsValid()
         {
-            //Arrange
-            
-            //Act
+            //arrange
+            int uploadMusicId = 1;
+            UploadMusic uploadedMusic = new UploadMusic();
+            _uploadMusicBLMock.Setup(x => x.GetUploadedMusicByIDAsync(uploadMusicId)).ReturnsAsync(uploadedMusic);
+            UploadMusicController uploadedMusicController = new UploadMusicController(_uploadMusicBLMock.Object);
 
-            //Assert
+            //act
+            var result = await uploadedMusicController.GetUploadedMusicByIDAsync(uploadMusicId);
+
+            //assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+            _uploadMusicBLMock.Verify(x => x.GetUploadedMusicByIDAsync(uploadMusicId));
         }
+
+        [Fact]
+        public async Task DeleteUploadedMusicAsync_ShouldReturnNoContent_WhenUploadMusicIsValid()
+        {
+            //arrange
+            int uploadedMusicID = 1;
+            UploadMusic uploadMusic = new UploadMusic();
+            _uploadMusicBLMock.Setup(x => x.DeleteUploadedMusicAsync(uploadMusic)).ReturnsAsync(uploadMusic);
+            UploadMusicController uploadedMusicController = new UploadMusicController(_uploadMusicBLMock.Object);
+
+            //act
+            var result = await uploadedMusicController.DeleteUploadedMusicAsync(uploadedMusicID);
+
+            //assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsInstanceOfType(result, typeof(NoContentResult));
+        }
+
+
+        [Fact]
+        public async Task GetUploadedMusicByUserIdAsync_ShouldReturn_OKObjectResult_WhenIDIsValid()
+        {
+            //arrange
+            int userID = 2;
+            UploadMusic uploadMusic = new UploadMusic();
+            _uploadMusicBLMock.Setup(x => x.GetUploadedMusicByIDAsync(userID)).ReturnsAsync(uploadMusic);
+            UploadMusicController uploadedMusicController = new UploadMusicController(_uploadMusicBLMock.Object);
+
+            //act
+            var result = await uploadedMusicController.GetUploadedMusicByIDAsync(userID);
+
+            //assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+        }
+
+        [Fact]
+        public async Task GetUploadedMusicByUserIdAsync_ShouldReturnNotFound_WhenUploadMusicIsNull()
+        {
+            //arrange
+            int userID = 2;
+            UploadMusic uploadMusic = null;
+            _uploadMusicBLMock.Setup(x => x.GetUploadedMusicByIDAsync(userID)).ReturnsAsync(uploadMusic);
+            UploadMusicController uploadedMusicController = new UploadMusicController(_uploadMusicBLMock.Object);
+
+            //act
+            var result = await uploadedMusicController.GetUploadedMusicByIDAsync(userID);
+
+            //assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsInstanceOfType(result, typeof(NotFoundResult));
+        }
+
+        [Fact]
+        public async Task GetUploadedMusicAsync_ShouldReturnOkObjectResult()
+        {
+            //arrange
+            List<UploadMusic> uploadMusics = new List<UploadMusic>();
+            _uploadMusicBLMock.Setup(x => x.GetUploadedMusicAsync()).ReturnsAsync(uploadMusics);
+            UploadMusicController uploadedMusicController = new UploadMusicController(_uploadMusicBLMock.Object);
+
+            //act
+            var result = await uploadedMusicController.GetUploadedMusicAsync();
+
+            //assert
+            Microsoft.VisualStudio.TestTools.UnitTesting.Assert.IsInstanceOfType(result, typeof(OkObjectResult));
+        }
+
+
     }
 }
