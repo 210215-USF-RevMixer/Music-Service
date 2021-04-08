83c9dd80feb4b08031adccbb8110a1e13453b519
hmittig <56450701+hmittig@users.noreply.github.com>
2021-04-08 11:30:17 -0400

    Added Music Playlist

--- UploadMusicService/MusicDL/IMusicRepoDB.cs
+++ UploadMusicService/MusicDL/IMusicRepoDB.cs
@@ -12,5 +12,11 @@ namespace MusicDL
         Task<UploadMusic> GetUploadedMusicByIDAsync(int id);
         Task<List<UploadMusic>> GetUploadedMusicByUserIDAsync(int userid);
         Task<UploadMusic> UpdateUploadedMusicAsync(UploadMusic uploadedMusic2BUpdated);
+
+        Task<MusicPlaylist> AddMusicPlaylistAsync(MusicPlaylist newMusicPlaylist);
+        Task<MusicPlaylist> DeleteMusicPlaylistAsync(MusicPlaylist musicPlaylist2BDeleted);
+        Task<MusicPlaylist> GetMusicPlaylistByIDAsync(int id);
+        Task<List<MusicPlaylist>> GetMusicPlaylistsAsync();
+        Task<MusicPlaylist> UpdateMusicPlaylistAsync(MusicPlaylist musicPlaylist2BUpdated);
     }
 }
--- UploadMusicService/MusicDL/UploadMusicDBContext.cs
+++ UploadMusicService/MusicDL/UploadMusicDBContext.cs
@@ -17,12 +17,16 @@ namespace MusicDL
         }
 
         public DbSet<UploadMusic> UploadMusic { get; set; }
-
+        public DbSet<MusicPlaylist> MusicPlaylist { get; set; }
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
             modelBuilder.Entity<UploadMusic>()
             .Property(x => x.Id)
             .ValueGeneratedOnAdd();
+
+            modelBuilder.Entity<MusicPlaylist>()
+               .Property(x => x.Id)
+               .ValueGeneratedOnAdd();
         }
     }
 }
