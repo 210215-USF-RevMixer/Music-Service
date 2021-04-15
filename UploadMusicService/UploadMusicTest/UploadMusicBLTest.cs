018ecaa3c8e42bf75629e1a65251c6fc176d10a6
ZMFrench <inbox@zachfrench.com>
2021-04-15 17:58:17 -0400

    tests for AddCommentAsync and GetCommentByMusicID

--- UploadMusicService/UploadMusicTest/UploadMusicBLTest.cs
+++ UploadMusicService/UploadMusicTest/UploadMusicBLTest.cs
@@ -24,11 +24,12 @@ namespace UploadMusicTest
         public async Task GetCommentsAsyncShouldReturnAllTheComments() 
         {
             var comments = new List<Comments> {new Comments(){Id = 1}, new Comments(){Id = 5}};
-            databaseMock.Setup(db => db.GetCommentsAsync());
+            databaseMock.Setup(db => db.GetCommentsAsync()).Returns(Task.FromResult<List<Comments>>(comments));
             var newMusicBL = new UploadedMusicBL(databaseMock.Object);
 
             var result = await newMusicBL.GetCommentsAsync();
 
+            Assert.NotNull(result);
             Assert.Equal(2, comments.Count());
             databaseMock.Verify(db => db.GetCommentsAsync());
         }
@@ -37,13 +38,47 @@ namespace UploadMusicTest
         public async Task GetCommentByIDAsyncShouldReturnTheCorrectComment()
         {
             var comment = new Comments() { Id = 1024, Comment = "test" };
-            databaseMock.Setup(db => db.GetCommentByIDAsync(It.IsAny<int>()));
+            databaseMock.Setup(db => db.GetCommentByIDAsync(It.IsAny<int>())).Returns(Task.FromResult<Comments>(comment));
             var newMusicBL = new UploadedMusicBL(databaseMock.Object);
 
             var result = await newMusicBL.GetCommentByIDAsync(1024);
 
             Assert.NotNull(result);
             Assert.Equal("test", result.Comment);
+            databaseMock.Verify(db => db.GetCommentByIDAsync(It.IsAny<int>()));
+        }
+
+        [Fact]
+        public async Task GetCommentsByMusicIDShouldReturnCorrectComments()
+        {
+            var comments = new List<Comments>
+            {
+                new Comments() { Id = 1, UploadMusicId = 10 },
+                new Comments() { Id = 3, UploadMusicId = 10 }
+            };
+            databaseMock.Setup(db => db.GetCommentsByMusicIDAsync(It.IsAny<int>())).Returns(Task.FromResult<List<Comments>>(comments));
+            var newMusicBL = new UploadedMusicBL(databaseMock.Object);
+
+            var result = await newMusicBL.GetCommentsByMusicIDAsync(10);
+
+            Assert.NotNull(result);
+            Assert.Equal(2, result.Count());
+            Assert.Equal(10, result.FirstOrDefault().UploadMusicId);
+            databaseMock.Verify(db => db.GetCommentsByMusicIDAsync(It.IsAny<int>()));
+        }
+
+        [Fact]
+        public async Task AddCommentAsyncShouldAddTheCommentToTheRepo()
+        {
+            var comment = new Comments() { Id = 5, Comment = "shrimp heaven now" };
+            databaseMock.Setup(db => db.AddCommentAsync(It.IsAny<Comments>())).Returns(Task.FromResult<Comments>(comment));
+            var newMusicBL = new UploadedMusicBL(databaseMock.Object);
+
+            var result = await newMusicBL.AddCommentAsync(comment);
+
+            Assert.NotNull(result);
+            Assert.Equal("shrimp heaven now", result.Comment);
+            databaseMock.Verify(db => db.AddCommentAsync(It.IsAny<Comments>()));
         }
 
 
